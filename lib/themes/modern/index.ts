8894e26e2c297fdefb77df908db5d5b7f77c06e4
RedstoneWizard22 <redstone@rwizard22.com>
2022-05-16 17:59:53 +0100

    🔥 Remove old components
    Going to rewrite them later :) They don't work anyway and just
    clutter my directory view

--- lib/themes/modern/index.ts
+++ lib/themes/modern/index.ts
@@ -1,31 +1,11 @@
 import type { Theme } from '$lib/_defines/types';
 
-import { buttonTheme } from './button';
-import { kbdTheme } from './kbd';
-import { overalyTheme } from './overlay';
-import { switchTheme } from './switch';
-import { drawerTheme } from './drawer';
-import { accordionTheme, accordionItemTheme } from './accordion';
-import { tabBarTheme, tabTheme } from './tabbar';
-import { tooltipTheme } from './tooltip';
-import { windowTheme } from './window';
 import { alertTheme } from './alert';
 import { checkboxTheme } from './checkbox';
 
 import { shared } from './shared';
 
 const theme: Theme = {
-	Overlay: overalyTheme,
-	Button: buttonTheme,
-	Kbd: kbdTheme,
-	Switch: switchTheme,
-	Drawer: drawerTheme,
-	Accordion: accordionTheme,
-	AccordionItem: accordionItemTheme,
-	TabBar: tabBarTheme,
-	Tab: tabTheme,
-	Tooltip: tooltipTheme,
-	Window: windowTheme,
 	Alert: alertTheme,
 	Checkbox: checkboxTheme,
 	shared,
--- lib/themes/modern/kbd.ts
+++
@@ -1,9 +0,0 @@
-import type { ComponentTheme } from '$lib/_defines/types';
-
-export const kbdTheme: ComponentTheme<'Kbd'> = {
-	variants: [],
-	defaultVariant: '',
-	core: {
-		base: 'bg-gray-100 border rounded p-0.5 px-1.5 font-medium border-gray-300 text-gray-600',
-	},
-};
